4bea168076372c6543b780d22fc2c74fb7ae7b16
Mário Henrique <marioh90@gmail.com>
2023-05-25 09:48:20 -0300

    finalizado com efeito fadeIn à imagem adicionada

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
     
     $('header button').click(function() {
-        alert('expandir formulário');
+        $('form').slideDown();
     })
+    $('#btn-cancelar').click(function() {
+        $('form').slideUp();
+    })
+    
     $('form').on('submit', function(e) {
         e.preventDefault();
+
+        const endImg = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${endImg}" />`).appendTo(novoItem);
+        $(`
+        <div class="overlay-imagem-link">
+            <a href="${endImg}" target="_blank" title="Ver imagem em tamnho real">
+                Ver imagem em tamnho real
+            </a>
+        </div>
+        `).appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1200);
+
+        $('#endereco-imagem-nova').val('')
     })
 })
