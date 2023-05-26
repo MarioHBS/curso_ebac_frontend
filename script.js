64b5fe9466e295f3966b28e852f56eeaf624d995
Mário Henrique <marioh90@gmail.com>
2023-05-26 16:18:33 -0300

    finalizado com a tarefa sendo riscada

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 $(document).ready(function() {
-    
     $('header button').click(function() {
         $('form').slideDown();
     })
-    $('#btn-cancelar').click(function() {
-        $('form').slideUp();
-    })
-    
+
     $('form').on('submit', function(e) {
         e.preventDefault();
 
-        const endImg = $('#endereco-imagem-nova').val();
-        const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${endImg}" />`).appendTo(novoItem);
+        const newTask = $('#txt-nova-tarefa').val();
+        const newLine = $(`<li style="display: none"></li>`);
         $(`
-        <div class="overlay-imagem-link">
-            <a href="${endImg}" target="_blank" title="Ver imagem em tamnho real">
-                Ver imagem em tamnho real
-            </a>
+        <div class="overlay-task">
+            <h3>${newTask}</h3>
         </div>
-        `).appendTo(novoItem);
+        `).appendTo(newLine);
 
-        $(novoItem).appendTo('ul');
-        $(novoItem).fadeIn(1200);
+        $(newLine).appendTo('ul');
+        $(newLine).fadeIn(1000);
 
-        $('#endereco-imagem-nova').val('')
+        $(newLine).click(function() {
+            $(newLine).css("text-decoration", "line-through")
+        });
+
+        $('#txt-nova-tarefa').val('')
     })
 })
