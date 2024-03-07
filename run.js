ff7df359fb2bbd58934b266530cab087276bdbca
Mário Henrique <marioh90@gmail.com>
2024-03-07 17:59:49 -0300

    adicionando ids aos componentes

--- run.js
+++ run.js
@@ -1,13 +1,16 @@
-fetch('http://api.github.com/users/mHBSantos')
-.then(function (response) {
-    return response.json();
-})
-.then(function (json) {
-    nameElm.innerText = json.name;
-    userElm.innerText = json.login;
-    // json.avatar_url;
-    // json.following;
-    // json.followers;
-    // json.public_repos;
-    // json.html_url;
-});
+$(document).ready(() =>
+    fetch('http://api.github.com/users/MarioHBS')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            nameElm.innerText = json.name;
+            userElm.innerText = json.login;
+            // json.avatar_url;
+            // json.following;
+            // json.followers;
+            // json.public_repos;
+            // json.html_url;
+        })
+);
+
