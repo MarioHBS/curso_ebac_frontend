aaddabdfdb4152f6e3d682c05d5d42dd5c0643ec
Mário Henrique <marioh90@gmail.com>
2024-03-25 11:39:19 -0300

    código Typescript pronto

---
+++ exercicio.ts
@@ -0,0 +1,8 @@
+const mult = (a: number, b: number) => {
+    return a*b;
+};
+const saudar = (nome: string) => `Olá, ${nome}`
+
+console.log(`13 * 11 é: ${mult(13, 11)}`)
+
+console.log(saudar("Adalberto"));
